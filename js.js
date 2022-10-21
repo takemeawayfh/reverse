1d6db56d8d4ae0d4ba73a15cab31a44df7707852
takemeawayfh <112930007+takemeawayfh@users.noreply.github.com>
2022-10-21 12:28:42 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function multi() {
+    let inp1 = document.getElementById('inp_1').value;
+
+    let result = inp1.split('').reverse().join('');
+    alert(result);
+}
---
+++ main.js
@@ -0,0 +1,69 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (lockBoard) return;
+    this.classList.add('flip');
+
+    if (this === firstCard) return;
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === 
+        secondCard.dataset.framework) {
+            disableCards();
+
+            return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
